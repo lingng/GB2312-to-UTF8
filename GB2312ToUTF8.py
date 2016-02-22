0ac8cea858f4a0c17513f5c418f76f4b51c1a8b8
Yuting ZHANG <lizlingng@gmail.com>
2016-02-21 16:36:25 -0800

    Add readme

--- GB2312ToUTF8.py
+++ GB2312ToUTF8.py
@@ -6,15 +6,32 @@ Output: Document with UTF-8 encoding
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
+import os
 
 def change_encode(fin):
+    # Get input file name fron the input path
     filename = fin.split('/')[-1]
 
+    # Get the position for the last occurance of '/' to split the path and the filename
     position = fin.rfind('/')
-    path = fin[:position]
+    if position < 0:
+        # Input file is in the current folder
+        path = os.path.dirname(os.path.realpath(fin))
+    else:
+        path = fin[:position]
 
+    # Expand "out_" prefix to the filename for the out file
     fout = "out_"+filename
+    while (os.path.isfile(path+'/'+fout)):
+        # Make sure to have one more "out_" prefix for the out file, so that no file will be overwritten
+        fout = "out_"+fout
 
+    # If the input file does not exist
+    if (not os.path.isfile(fin)):
+        print "Error: No such file!"
+        sys.exit(2)
+
+    # Convert encoding
     content = open(fin, 'r').read().decode('gb2312')
     outpath = path+'/'+fout
     fo = open(outpath, 'w')
@@ -23,12 +40,14 @@ def change_encode(fin):
 
 
 def main(argv):
+    # Wrong argument number
     if len(argv) != 2:
         print("Error: Wrong argument number.")
         sys.exit(2)
 
     fin = argv[1]
 
+    # Not txt file
     if not fin.endswith('.txt'):
         print("Error: Please choose a txt file. Format: python GB2312ToUTF8.py path/to/input/text_file.txt")
         sys.exit(2)
---
+++ readme.md
@@ -0,0 +1,23 @@
+##GB2312 to UTF-8 Converter
+====
+####Introduction
+Allow users to convert a text file with GB2312 encoding to UTF-8 encoding.
+
+####Usage
+Please use the following command line to run the script:
+
+	python GB2312ToUTF8.py path/to/input/text_file.txt
+	
+* The output file will be in the same folder as the input file. 
+* The name for the output file will have a prefix "out_". 
+If there already exist a file with the same name as "out_text_file.txt", the script will automatically expend one more "out\_" prefix in front of the name. So no file will be overwritten.
+
+====
+#### 简介
+用户可使用此脚本将GB2312编码的txt文件转换为UTF8编码的txt文件。
+#### 使用方法介绍
+请在终端使用如下命令执行该脚本：
+	
+	python GB2312ToUTF8.py path/to/input/text_file.txt
+	
+输出文件与输入文件在同一文件夹中，输出文件名将包含"out_"前缀，以此与输入文件区分开。
